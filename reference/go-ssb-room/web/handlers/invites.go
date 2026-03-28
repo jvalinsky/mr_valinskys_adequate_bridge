1d176e14515855cf1bfb7f06716b6e893483b0f5
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-03-28 17:05:40 -0400

    Fix Planetary crash: improve invite JSON compatibility

---
+++ reference/go-ssb-room/web/handlers/invites.go
@@ -0,0 +1,413 @@
+// SPDX-FileCopyrightText: 2021 The NGI Pointer Secure-Scuttlebutt Team of 2020/2021
+//
+// SPDX-License-Identifier: MIT
+
+package handlers
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"html/template"
+	"image/color"
+	"net/http"
+	"net/url"
+
+	"github.com/gorilla/csrf"
+	"github.com/skip2/go-qrcode"
+	"go.mindeco.de/http/render"
+	"go.mindeco.de/log/level"
+	"go.mindeco.de/logging"
+
+	refs "github.com/ssbc/go-ssb-refs"
+	"github.com/ssbc/go-ssb-room/v2/internal/network"
+	"github.com/ssbc/go-ssb-room/v2/roomdb"
+	"github.com/ssbc/go-ssb-room/v2/web"
+	weberrors "github.com/ssbc/go-ssb-room/v2/web/errors"
+	"github.com/ssbc/go-ssb-room/v2/web/router"
+)
+
+type inviteHandler struct {
+	render      *render.Renderer
+	urlTo       web.URLMaker
+	networkInfo network.ServerEndpointDetails
+
+	invites       roomdb.InvitesService
+	pinnedNotices roomdb.PinnedNoticesService
+	config        roomdb.RoomConfig
+	deniedKeys    roomdb.DeniedKeysService
+}
+
+func (h inviteHandler) buildJoinRoomURI(token string, userAgent string) template.URL {
+	queryVals := make(url.Values)
+	queryVals.Set("action", "claim-http-invite")
+	queryVals.Set("invite", token)
+
+	submissionURL := h.urlTo(router.CompleteInviteConsume)
+	queryVals.Set("postTo", submissionURL.String())
+
+	joinRoomURI := url.URL{
+		Scheme:   "ssb",
+		Opaque:   "experimental",
+		RawQuery: queryVals.Encode(),
+	}
+
+	return template.URL(web.StringifySSBURI(&joinRoomURI, userAgent))
+}
+
+// switch between JSON and HTML responses
+func (h inviteHandler) presentFacade(rw http.ResponseWriter, req *http.Request) {
+	enc := req.URL.Query().Get("encoding")
+	if enc == "json" {
+		h.presentFacadeAsJSON(rw, req)
+		return
+	}
+
+	h.render.HTML("invite/facade.tmpl", h.presentFacadeAsHTML)(rw, req)
+}
+
+func (h inviteHandler) presentFacadeAsJSON(rw http.ResponseWriter, req *http.Request) {
+	logger := logging.FromContext(req.Context())
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+
+	enc := json.NewEncoder(rw)
+
+	// make sure token is still valid
+	token := req.URL.Query().Get("token")
+	_, err := h.invites.GetByToken(req.Context(), token)
+	if err != nil {
+		// send a json error back
+		data := struct {
+			Status string `json:"status"`
+			Error  string `json:"error"`
+		}{"failed", err.Error()}
+		if err := enc.Encode(data); err != nil {
+			level.Warn(logger).Log("event", "sending json error failed", "err", err)
+		}
+		return
+	}
+
+	// send them on to the next step
+	postTo := h.urlTo(router.CompleteInviteConsume)
+	data := struct {
+		Status string `json:"status"`
+		Invite string `json:"invite"`
+		PostTo string `json:"postTo"`
+	}{"success", token, postTo.String()}
+	if err := enc.Encode(data); err != nil {
+		level.Warn(logger).Log("event", "sending json response failed", "err", err)
+	}
+}
+
+func (h inviteHandler) presentFacadeAsHTML(rw http.ResponseWriter, req *http.Request) (interface{}, error) {
+	token := req.URL.Query().Get("token")
+
+	_, err := h.invites.GetByToken(req.Context(), token)
+	if err != nil {
+		if errors.Is(err, roomdb.ErrNotFound) {
+			return nil, weberrors.ErrNotFound{What: "invite"}
+		}
+		return nil, err
+	}
+
+	notice, err := h.pinnedNotices.Get(req.Context(), roomdb.NoticeDescription, "en-GB")
+	if err != nil {
+		return nil, fmt.Errorf("failed to find room's description: %w", err)
+	}
+
+	joinRoomURI := h.buildJoinRoomURI(token, req.UserAgent())
+
+	fallbackURL := h.urlTo(router.CompleteInviteFacadeFallback, "token", token)
+
+	// generate a QR code with the token inside so that you can open it easily in a supporting mobile app
+	thisURL := req.URL
+	thisURL.Host = h.networkInfo.Domain
+	thisURL.Scheme = "https"
+	if h.networkInfo.Development {
+		thisURL.Scheme = "http"
+		thisURL.Host += fmt.Sprintf(":%d", h.networkInfo.PortHTTPS)
+	}
+	qrCode, err := qrcode.New(thisURL.String(), qrcode.Medium)
+	if err != nil {
+		return nil, err
+	}
+
+	qrCode.BackgroundColor = color.Transparent // transparent to fit into the page
+	qrCode.ForegroundColor = color.Black
+
+	qrCodeData, err := qrCode.PNG(-5)
+	if err != nil {
+		return nil, err
+	}
+	qrURI := "data:image/png;base64," + base64.StdEncoding.EncodeToString(qrCodeData)
+
+	return map[string]interface{}{
+		csrf.TemplateTag: csrf.TemplateField(req),
+		"RoomTitle":      notice.Title,
+		"JoinRoomURI":    joinRoomURI,
+		"FallbackURL":    fallbackURL,
+		"QRCodeURI":      template.URL(qrURI),
+	}, nil
+}
+
+func (h inviteHandler) presentFacadeFallback(rw http.ResponseWriter, req *http.Request) (interface{}, error) {
+	token := req.URL.Query().Get("token")
+
+	_, err := h.invites.GetByToken(req.Context(), token)
+	if err != nil {
+		if errors.Is(err, roomdb.ErrNotFound) {
+			return nil, weberrors.ErrNotFound{What: "invite"}
+		}
+		return nil, err
+	}
+
+	insertURL := h.urlTo(router.CompleteInviteInsertID, "token", token)
+
+	return map[string]interface{}{
+		csrf.TemplateTag: csrf.TemplateField(req),
+		"InsertURL":      insertURL,
+	}, nil
+}
+
+func (h inviteHandler) presentInsert(rw http.ResponseWriter, req *http.Request) (interface{}, error) {
+	token := req.URL.Query().Get("token")
+
+	_, err := h.invites.GetByToken(req.Context(), token)
+	if err != nil {
+		if errors.Is(err, roomdb.ErrNotFound) {
+			return nil, weberrors.ErrNotFound{What: "invite"}
+		}
+		return nil, err
+	}
+
+	return map[string]interface{}{
+		csrf.TemplateTag: csrf.TemplateField(req),
+		"Token":          token,
+	}, nil
+}
+
+type inviteConsumePayload struct {
+	ID     refs.FeedRef `json:"id"`
+	Invite string       `json:"invite"`
+}
+
+func (h inviteHandler) consume(rw http.ResponseWriter, req *http.Request) {
+	logger := logging.FromContext(req.Context())
+
+	var (
+		token     string
+		newMember refs.FeedRef
+		resp      inviteConsumeResponder
+	)
+
+	ct := req.Header.Get("Content-Type")
+	switch ct {
+	case "application/json":
+		resp = newinviteConsumeJSONResponder(rw)
+
+		var body inviteConsumePayload
+
+		level.Debug(logger).Log("event", "handling json body")
+		err := json.NewDecoder(req.Body).Decode(&body)
+		if err != nil {
+			err = fmt.Errorf("consume body contained invalid json: %w", err)
+			resp.SendError(err)
+			return
+		}
+
+		newMember = body.ID
+		token = body.Invite
+	case "application/x-www-form-urlencoded":
+		resp = newinviteConsumeHTMLResponder(h.render, rw, req)
+
+		if err := req.ParseForm(); err != nil {
+			err = weberrors.ErrBadRequest{Where: "form data", Details: err}
+			resp.SendError(err)
+			return
+		}
+
+		token = req.FormValue("invite")
+
+		parsedID, err := refs.ParseFeedRef(req.FormValue("id"))
+		if err != nil {
+			err = weberrors.ErrBadRequest{Where: "id", Details: err}
+			resp.SendError(err)
+			return
+		}
+		newMember = parsedID
+	default:
+		http.Error(rw, fmt.Sprintf("unhandled Content-Type (%q)", ct), http.StatusBadRequest)
+		return
+	}
+
+	// before consuming the invite: check if the invitee is banned
+	if h.deniedKeys.HasFeed(req.Context(), newMember) {
+		resp.SendError(weberrors.ErrDenied)
+		return
+	}
+
+	resp.UpdateMultiserverAddr(h.networkInfo.MultiserverAddress())
+
+	inv, err := h.invites.Consume(req.Context(), token, newMember)
+	if err != nil {
+		if errors.Is(err, roomdb.ErrNotFound) {
+			resp.SendError(weberrors.ErrNotFound{What: "invite"})
+			return
+		}
+		resp.SendError(err)
+		return
+	}
+	log := logging.FromContext(req.Context())
+	level.Info(log).Log("event", "invite consumed", "id", inv.ID, "ref", newMember.ShortSigil())
+
+	resp.SendSuccess()
+}
+
+// inviteConsumeResponder is supposed to handle different encoding types transparently.
+// It either sends the rooms multiaddress on success or an error.
+type inviteConsumeResponder interface {
+	SendSuccess()
+	SendError(error)
+
+	UpdateMultiserverAddr(string)
+}
+
+// inviteConsumeJSONResponse dictates the field names and format of the JSON response for the inviteConsume web endpoint
+type inviteConsumeJSONResponse struct {
+	Status string `json:"status"`
+
+	// Address is the key name expected by older clients (Oasis/Room1.0)
+	Address string `json:"address"`
+
+	// MultiserverAddress is the key name specified by the modern Room 2.0 tunneling spec
+	MultiserverAddress string `json:"multiserverAddress"`
+}
+
+// handles JSON responses
+type inviteConsumeJSONResponder struct {
+	enc *json.Encoder
+
+	multiservAddr string
+}
+
+func newinviteConsumeJSONResponder(rw http.ResponseWriter) inviteConsumeResponder {
+	rw.Header().Set("Content-Type", "application/json")
+	return &inviteConsumeJSONResponder{
+		enc: json.NewEncoder(rw),
+	}
+}
+
+func (json *inviteConsumeJSONResponder) UpdateMultiserverAddr(msaddr string) {
+	json.multiservAddr = msaddr
+}
+
+func (json inviteConsumeJSONResponder) SendSuccess() {
+	var resp = inviteConsumeJSONResponse{
+		Status:             "success",
+		Address:            json.multiservAddr,
+		MultiserverAddress: json.multiservAddr,
+	}
+	json.enc.Encode(resp)
+}
+
+func (json inviteConsumeJSONResponder) SendError(err error) {
+	json.enc.Encode(struct {
+		Status string `json:"status"`
+		Error  string `json:"error"`
+	}{"error", err.Error()})
+}
+
+// handles HTML responses
+type inviteConsumeHTMLResponder struct {
+	renderer *render.Renderer
+	rw       http.ResponseWriter
+	req      *http.Request
+
+	multiservAddr string
+}
+
+func newinviteConsumeHTMLResponder(r *render.Renderer, rw http.ResponseWriter, req *http.Request) inviteConsumeResponder {
+	return &inviteConsumeHTMLResponder{
+		renderer: r,
+		rw:       rw,
+		req:      req,
+	}
+}
+
+func (html *inviteConsumeHTMLResponder) UpdateMultiserverAddr(msaddr string) {
+	html.multiservAddr = msaddr
+}
+
+func (html inviteConsumeHTMLResponder) SendSuccess() {
+	err := html.renderer.Render(html.rw, html.req, "invite/consumed.tmpl", http.StatusOK, struct {
+		MultiserverAddress string
+	}{(html.multiservAddr)})
+	if err != nil {
+		logger := logging.FromContext(html.req.Context())
+		level.Warn(logger).Log("event", "render failed", "err", err)
+	}
+}
+
+func (html inviteConsumeHTMLResponder) SendError(err error) {
+	html.renderer.Error(html.rw, html.req, http.StatusInternalServerError, err)
+}
+
+func (h inviteHandler) createOpenMode(rw http.ResponseWriter, r *http.Request) {
+	switch r.Header.Get("Accept") {
+	case "application/json":
+		if r.Method != http.MethodPost {
+			h.render.Error(rw, r, http.StatusBadRequest, errors.New("invalid method"))
+		}
+		h.createOpenModeJSON(rw, r)
+	default:
+		if r.Method != http.MethodGet {
+			h.render.Error(rw, r, http.StatusBadRequest, errors.New("invalid method"))
+		}
+		h.render.HTML("admin/invite-created.tmpl", h.createOpenModeHTML)(rw, r)
+	}
+}
+
+func (h inviteHandler) createOpenModeHTML(rw http.ResponseWriter, req *http.Request) (interface{}, error) {
+	ctx := req.Context()
+
+	token, err := h.invites.Create(ctx, -1)
+	if err != nil {
+		return nil, err
+	}
+
+	facadeURL := h.urlTo(router.CompleteInviteFacade, "token", token)
+
+	return map[string]interface{}{
+		"FacadeURL": facadeURL.String(),
+	}, nil
+}
+
+func (h inviteHandler) createOpenModeJSON(rw http.ResponseWriter, req *http.Request) {
+	logger := logging.FromContext(req.Context())
+	ctx := req.Context()
+	enc := json.NewEncoder(rw)
+
+	token, err := h.invites.Create(ctx, -1)
+	if err != nil {
+		data := struct {
+			Status string `json:"status"`
+			Error  string `json:"error"`
+		}{"failed", err.Error()}
+		rw.WriteHeader(http.StatusInternalServerError)
+		if err := enc.Encode(data); err != nil {
+			level.Warn(logger).Log("event", "sending json error failed", "err", err)
+		}
+		return
+	}
+
+	response := map[string]string{
+		"url": h.urlTo(router.CompleteInviteFacade, "token", token).String(),
+	}
+
+	if err := enc.Encode(response); err != nil {
+		level.Warn(logger).Log("event", "sending json response failed", "err", err)
+	}
+}
---
+++ reference/go-ssb-room/web/handlers/invites_test.go
@@ -0,0 +1,384 @@
+// SPDX-FileCopyrightText: 2021 The NGI Pointer Secure-Scuttlebutt Team of 2020/2021
+//
+// SPDX-License-Identifier: MIT
+
+package handlers
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	refs "github.com/ssbc/go-ssb-refs"
+	"github.com/ssbc/go-ssb-room/v2/roomdb"
+	weberrors "github.com/ssbc/go-ssb-room/v2/web/errors"
+	"github.com/ssbc/go-ssb-room/v2/web/router"
+	"github.com/ssbc/go-ssb-room/v2/web/webassert"
+)
+
+func TestInviteShowAcceptForm(t *testing.T) {
+	ts := setup(t)
+
+	t.Run("token doesnt exist", func(t *testing.T) {
+		a, r := assert.New(t), require.New(t)
+
+		testToken := "nonexistant-test-token"
+		acceptURL404 := ts.URLTo(router.CompleteInviteFacade, "token", testToken)
+		r.NotNil(acceptURL404)
+
+		// prep the mocked db for http:404
+		ts.InvitesDB.GetByTokenReturns(roomdb.Invite{}, roomdb.ErrNotFound)
+
+		// request the form
+		doc, resp := ts.Client.GetHTML(acceptURL404)
+		// 500 until https://github.com/ssbc/go-ssb-room/issues/66 is fixed
+		a.Equal(http.StatusInternalServerError, resp.Code)
+
+		// check database calls
+		r.EqualValues(1, ts.InvitesDB.GetByTokenCallCount())
+		_, tokenFromArg := ts.InvitesDB.GetByTokenArgsForCall(0)
+		a.Equal(testToken, tokenFromArg)
+
+		// fix #66
+		// assertLocalized(t, doc, []localizedElement{
+		// 	{"#welcome", "AuthFallbackWelcome"},
+		// 	{"title", "AuthFallbackTitle"},
+		// })
+		gotErr := doc.Find("#errBody").Text()
+		wantErr := weberrors.ErrNotFound{What: "invite"}
+		a.EqualError(wantErr, gotErr)
+	})
+
+	t.Run("token DOES exist", func(t *testing.T) {
+		a, r := assert.New(t), require.New(t)
+
+		testToken := "existing-test-token"
+		validAcceptURL := ts.URLTo(router.CompleteInviteFacade, "token", testToken)
+
+		// prep the mocked db for http:200
+		fakeExistingInvite := roomdb.Invite{ID: 1234}
+		ts.InvitesDB.GetByTokenReturns(fakeExistingInvite, nil)
+
+		// request the form
+		doc, resp := ts.Client.GetHTML(validAcceptURL)
+		a.Equal(http.StatusOK, resp.Code)
+
+		// check database calls
+		r.EqualValues(2, ts.InvitesDB.GetByTokenCallCount())
+		_, tokenFromArg := ts.InvitesDB.GetByTokenArgsForCall(1)
+		a.Equal(testToken, tokenFromArg)
+
+		webassert.Localized(t, doc, []webassert.LocalizedElement{
+			{"#claim-invite-uri", "InviteFacadeJoin"},
+			{"title", "InviteFacadeTitle"},
+		})
+
+		// Fallback URL in data-href-fallback
+		fallbackURL := ts.URLTo(router.CompleteInviteFacadeFallback, "token", testToken)
+		joinDataHrefFallback, ok := doc.Find("#claim-invite-uri").Attr("data-href-fallback")
+		a.Equal(fallbackURL.String(), joinDataHrefFallback)
+		a.True(ok)
+
+		// ssb-uri in href
+		joinDataHref, ok := doc.Find("#claim-invite-uri").Attr("href")
+		a.True(ok)
+		joinURI, err := url.Parse(joinDataHref)
+		r.NoError(err)
+
+		a.Equal("ssb", joinURI.Scheme)
+		a.Equal("experimental", joinURI.Opaque)
+
+		params := joinURI.Query()
+		a.Equal("claim-http-invite", params.Get("action"))
+
+		inviteParam := params.Get("invite")
+		a.Equal(testToken, inviteParam)
+
+		postTo := params.Get("postTo")
+		expectedConsumeInviteURL := ts.URLTo(router.CompleteInviteConsume)
+		a.Equal(expectedConsumeInviteURL.String(), postTo)
+	})
+}
+
+func TestInviteShowAcceptFormOnAndroidChrome(t *testing.T) {
+	ts := setup(t)
+
+	a, r := assert.New(t), require.New(t)
+
+	testToken := "existing-test-token"
+	validAcceptURL := ts.URLTo(router.CompleteInviteFacade, "token", testToken)
+
+	// Mimic Android Chrome
+	var uaHeader = make(http.Header)
+	uaHeader.Set("User-Agent", "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5 Build/MOB30H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.133 Mobile Safari/537.36")
+	ts.Client.SetHeaders(uaHeader)
+
+	// prep the mocked db for http:200
+	fakeExistingInvite := roomdb.Invite{ID: 1234}
+	ts.InvitesDB.GetByTokenReturns(fakeExistingInvite, nil)
+
+	// request the form
+	doc, resp := ts.Client.GetHTML(validAcceptURL)
+	a.Equal(http.StatusOK, resp.Code)
+
+	// check database calls
+	r.EqualValues(1, ts.InvitesDB.GetByTokenCallCount())
+	_, tokenFromArg := ts.InvitesDB.GetByTokenArgsForCall(0)
+	a.Equal(testToken, tokenFromArg)
+
+	webassert.Localized(t, doc, []webassert.LocalizedElement{
+		{"#claim-invite-uri", "InviteFacadeJoin"},
+		{"title", "InviteFacadeTitle"},
+	})
+
+	// ssb-uri in href
+	joinDataHref, ok := doc.Find("#claim-invite-uri").Attr("href")
+	a.True(ok)
+	joinURI, err := url.Parse(joinDataHref)
+	r.NoError(err)
+
+	a.Equal("intent", joinURI.Scheme)
+	a.Equal("experimental", joinURI.Host)
+
+	params := joinURI.Query()
+	a.Equal("claim-http-invite", params.Get("action"))
+
+	inviteParam := params.Get("invite")
+	a.Equal(testToken, inviteParam)
+
+	postTo := params.Get("postTo")
+	expectedConsumeInviteURL := ts.URLTo(router.CompleteInviteConsume)
+	a.Equal(expectedConsumeInviteURL.String(), postTo)
+
+	frag := joinURI.Fragment
+	a.Equal("Intent;scheme=ssb;end;", frag)
+}
+
+func TestInviteConsumeInviteHTTP(t *testing.T) {
+	ts := setup(t)
+	a, r := assert.New(t), require.New(t)
+
+	testToken := "existing-test-token-2"
+	validAcceptURL := ts.URLTo(router.CompleteInviteInsertID, "token", testToken)
+	testInvite := roomdb.Invite{ID: 4321}
+	ts.InvitesDB.GetByTokenReturns(testInvite, nil)
+
+	// request the form (for a valid csrf token)
+	doc, resp := ts.Client.GetHTML(validAcceptURL)
+	a.Equal(http.StatusOK, resp.Code)
+
+	form := doc.Find("form#inviteConsume")
+	r.Equal(1, form.Length())
+
+	consumeInviteURLString, has := form.Attr("action")
+	a.True(has, "form should have an action attribute")
+	expectedConsumeInviteURL := ts.URLTo(router.CompleteInviteConsume)
+	a.Equal(expectedConsumeInviteURL.String(), consumeInviteURLString)
+
+	webassert.CSRFTokenPresent(t, form)
+	webassert.ElementsInForm(t, form, []webassert.FormElement{
+		{Name: "invite", Type: "hidden", Value: testToken},
+		{Name: "id", Type: "text"},
+	})
+
+	// get the corresponding token from the page
+	csrfTokenElem := form.Find(`input[name="gorilla.csrf.Token"]`)
+	a.Equal(1, csrfTokenElem.Length())
+	csrfName, has := csrfTokenElem.Attr("name")
+	a.True(has, "should have a name attribute")
+	csrfValue, has := csrfTokenElem.Attr("value")
+	a.True(has, "should have value attribute")
+
+	// create the consume request
+	testNewMember, err := refs.NewFeedRefFromBytes(bytes.Repeat([]byte{1}, 32), refs.RefAlgoFeedSSB1)
+	if err != nil {
+		t.Error(err)
+	}
+	consumeVals := url.Values{
+		"invite": []string{testToken},
+		"id":     []string{testNewMember.String()},
+
+		csrfName: []string{csrfValue},
+	}
+
+	// construct the consume endpoint url
+	consumeInviteURL := ts.URLTo(router.CompleteInviteConsume)
+
+	// construct the header with the Referer or csrf check
+	var csrfCookieHeader = http.Header(map[string][]string{})
+	csrfCookieHeader.Set("Referer", "https://localhost")
+	ts.Client.SetHeaders(csrfCookieHeader)
+
+	// prepare the mock
+	ts.InvitesDB.ConsumeReturns(testInvite, nil)
+
+	// send the POST
+	resp = ts.Client.PostForm(consumeInviteURL, consumeVals)
+	a.Equal(http.StatusOK, resp.Code, "wrong HTTP status code for sign in")
+
+	// check how consume was called
+	r.EqualValues(1, ts.InvitesDB.ConsumeCallCount())
+	_, tokenFromArg, newMemberRef := ts.InvitesDB.ConsumeArgsForCall(0)
+	a.Equal(testToken, tokenFromArg)
+	a.True(newMemberRef.Equal(testNewMember))
+}
+
+func TestInviteConsumeInviteJSON(t *testing.T) {
+	ts := setup(t)
+	a, r := assert.New(t), require.New(t)
+
+	testToken := "existing-test-token-2"
+
+	testInvite := roomdb.Invite{ID: 4321}
+	ts.InvitesDB.GetByTokenReturns(testInvite, nil)
+
+	// check if the token is still valid
+	checkInviteURL := ts.URLTo(router.CompleteInviteFacade)
+	qvals := url.Values{
+		"token":    []string{testToken},
+		"encoding": []string{"json"},
+	}
+	checkInviteURL.RawQuery = qvals.Encode()
+
+	// send the request and check the json
+	resp := ts.Client.GetBody(checkInviteURL)
+	result := resp.Result()
+	a.Equal(http.StatusOK, result.StatusCode)
+
+	var reply struct {
+		Invite string
+		PostTo string
+	}
+	err := json.NewDecoder(result.Body).Decode(&reply)
+	r.NoError(err)
+
+	// construct the consume endpoint url
+	consumeInviteURL := ts.URLTo(router.CompleteInviteConsume)
+
+	a.Equal(consumeInviteURL.String(), reply.PostTo, "wrong postTo in JSON body")
+	a.Equal(testToken, reply.Invite, "wrong invite token")
+
+	// create the consume request
+	testNewMember, err := refs.NewFeedRefFromBytes(bytes.Repeat([]byte{1}, 32), refs.RefAlgoFeedSSB1)
+	if err != nil {
+		t.Error(err)
+	}
+	var consume inviteConsumePayload
+	consume.Invite = testToken
+	consume.ID = testNewMember
+
+	// prepare the mock
+	ts.InvitesDB.ConsumeReturns(testInvite, nil)
+
+	// send the POST
+	resp = ts.Client.SendJSON(consumeInviteURL, consume)
+	a.Equal(http.StatusOK, resp.Code, "wrong HTTP status code for sign in")
+
+	// check how consume was called
+	r.EqualValues(1, ts.InvitesDB.ConsumeCallCount())
+	_, tokenFromArg, newMemberRef := ts.InvitesDB.ConsumeArgsForCall(0)
+	a.Equal(testToken, tokenFromArg)
+	a.True(newMemberRef.Equal(testNewMember))
+
+	var jsonConsumeResp inviteConsumeJSONResponse
+	err = json.NewDecoder(resp.Body).Decode(&jsonConsumeResp)
+	r.NoError(err)
+
+	a.Equal("success", jsonConsumeResp.Status)
+
+	gotRA := jsonConsumeResp.MultiserverAddress
+	a.True(strings.HasPrefix(gotRA, "net:localhost:8008~shs:"), "not for the test host: %s", gotRA)
+	a.True(strings.HasSuffix(gotRA, base64.StdEncoding.EncodeToString(ts.NetworkInfo.RoomID.PubKey())), "public key missing? %s", gotRA)
+}
+
+func TestInviteConsumptionDenied(t *testing.T) {
+	ts := setup(t)
+	a, r := assert.New(t), require.New(t)
+
+	testToken := "existing-test-token-2"
+	validAcceptURL := ts.URLTo(router.CompleteInviteFacade, "token", testToken)
+	r.NotNil(validAcceptURL)
+
+	testInvite := roomdb.Invite{ID: 4321}
+	ts.InvitesDB.GetByTokenReturns(testInvite, nil)
+
+	ts.DeniedKeysDB.HasFeedReturns(true)
+
+	// create the consume request
+	testNewMember, err := refs.NewFeedRefFromBytes(bytes.Repeat([]byte{1}, 32), refs.RefAlgoFeedSSB1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	var consume inviteConsumePayload
+	consume.Invite = testToken
+	consume.ID = testNewMember
+
+	// construct the consume endpoint url
+	consumeInviteURL := ts.URLTo(router.CompleteInviteConsume)
+	r.NotNil(consumeInviteURL)
+
+	// prepare the mock
+	ts.InvitesDB.ConsumeReturns(testInvite, nil)
+
+	// send the POST
+	resp := ts.Client.SendJSON(consumeInviteURL, consume)
+
+	// decode the json response
+	var jsonConsumeResp inviteConsumeJSONResponse
+	err = json.NewDecoder(resp.Body).Decode(&jsonConsumeResp)
+	r.NoError(err)
+
+	// json response should indicate an error for the denied key
+	a.Equal("error", jsonConsumeResp.Status)
+
+	// invite should not be consumed
+	r.EqualValues(0, ts.InvitesDB.ConsumeCallCount())
+}
+
+func TestOpenModeCreateInviteHTML(t *testing.T) {
+	ts := setup(t)
+	r := require.New(t)
+
+	someToken := "fake-token"
+	ts.InvitesDB.CreateReturns(someToken, nil)
+
+	doc, resp := ts.Client.GetHTML(ts.URLTo(router.OpenModeCreateInvite))
+	r.Equal(http.StatusOK, resp.Code)
+
+	facadeLink := doc.Find("#invite-facade-link")
+	r.NotNil(facadeLink)
+	r.Contains(facadeLink.AttrOr("href", ""), someToken)
+	r.Contains(facadeLink.Text(), someToken)
+}
+
+func TestOpenModeCreateInviteJSON(t *testing.T) {
+	ts := setup(t)
+	r := require.New(t)
+
+	someToken := "fake-token"
+	ts.InvitesDB.CreateReturns(someToken, nil)
+
+	req, err := http.NewRequest("POST", ts.URLTo(router.OpenModeCreateInvite).String(), nil)
+	r.NoError(err)
+
+	req.Header.Set("Accept", "application/json")
+
+	recorder := httptest.NewRecorder()
+	ts.Mux.ServeHTTP(recorder, req)
+	r.Equal(http.StatusOK, recorder.Code)
+
+	response := map[string]string{}
+	err = json.Unmarshal(recorder.Body.Bytes(), &response)
+	r.NoError(err)
+
+	require.Contains(t, response["url"], someToken)
+}
