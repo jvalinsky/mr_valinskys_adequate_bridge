a2c5739eb123d310bc4599c7d92e83d6117e9142
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-04-02 01:16:40 -0400

    fix(firehose): decode commit frames via cid-native wire structs

--- pkg/atproto/firehose/firehose.go
+++ pkg/atproto/firehose/firehose.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/ipfs/go-cid"
 	cbornode "github.com/ipfs/go-ipld-cbor"
 	"github.com/jvalinsky/mr_valinskys_adequate_bridge/pkg/atproto"
+	"github.com/jvalinsky/mr_valinskys_adequate_bridge/pkg/atproto/lexutil"
 )
 
 const (
@@ -33,9 +35,35 @@ type Event struct {
 	Error        *ErrorFrame
 }
 
+// commitWire and repoOpWire decode firehose CBOR frames using cid.Cid
+// directly to avoid refmt atlas issues with LexLink alias types.
+type commitWire struct {
+	Blobs    []cid.Cid     `json:"blobs" refmt:"blobs"`
+	Blocks   []byte        `json:"blocks,omitempty" refmt:"blocks,omitempty"`
+	Commit   cid.Cid       `json:"commit" refmt:"commit"`
+	Ops      []*repoOpWire `json:"ops" refmt:"ops"`
+	PrevData *cid.Cid      `json:"prevData,omitempty" refmt:"prevData,omitempty"`
+	Rebase   bool          `json:"rebase" refmt:"rebase"`
+	Repo     string        `json:"repo" refmt:"repo"`
+	Rev      string        `json:"rev" refmt:"rev"`
+	Seq      int64         `json:"seq" refmt:"seq"`
+	Since    *string       `json:"since,omitempty" refmt:"since,omitempty"`
+	Time     string        `json:"time" refmt:"time"`
+	TooBig   bool          `json:"tooBig" refmt:"tooBig"`
+}
+
+type repoOpWire struct {
+	Action string   `json:"action" refmt:"action"`
+	Cid    *cid.Cid `json:"cid" refmt:"cid"`
+	Path   string   `json:"path" refmt:"path"`
+	Prev   *cid.Cid `json:"prev,omitempty" refmt:"prev,omitempty"`
+}
+
 func init() {
 	cbornode.RegisterCborType(EventHeader{})
 	cbornode.RegisterCborType(ErrorFrame{})
+	cbornode.RegisterCborType(commitWire{})
+	cbornode.RegisterCborType(repoOpWire{})
 }
 
 func ReadEvent(r io.Reader) (*Event, error) {
@@ -60,11 +88,11 @@ func ReadEvent(r io.Reader) (*Event, error) {
 
 	switch header.MsgType {
 	case "#commit":
-		var commit atproto.SyncSubscribeRepos_Commit
-		if err := cbornode.DecodeReader(r, &commit); err != nil {
+		var wire commitWire
+		if err := cbornode.DecodeReader(r, &wire); err != nil {
 			return nil, fmt.Errorf("decode commit event: %w", err)
 		}
-		event.RepoCommit = &commit
+		event.RepoCommit = convertCommitWire(&wire)
 	case "#sync":
 		var sync atproto.SyncSubscribeRepos_Sync
 		if err := cbornode.DecodeReader(r, &sync); err != nil {
@@ -94,4 +122,58 @@ func ReadEvent(r io.Reader) (*Event, error) {
 	}
 
 	return event, nil
+}
+
+func convertCommitWire(wire *commitWire) *atproto.SyncSubscribeRepos_Commit {
+	if wire == nil {
+		return nil
+	}
+
+	out := &atproto.SyncSubscribeRepos_Commit{
+		Blocks: lexutil.LexBytes(wire.Blocks),
+		Commit: lexutil.LexLink(wire.Commit),
+		Rebase: wire.Rebase,
+		Repo:   wire.Repo,
+		Rev:    wire.Rev,
+		Seq:    wire.Seq,
+		Since:  wire.Since,
+		Time:   wire.Time,
+		TooBig: wire.TooBig,
+	}
+
+	if len(wire.Blobs) > 0 {
+		out.Blobs = make([]lexutil.LexLink, 0, len(wire.Blobs))
+		for _, blob := range wire.Blobs {
+			out.Blobs = append(out.Blobs, lexutil.LexLink(blob))
+		}
+	}
+
+	if wire.PrevData != nil {
+		prev := lexutil.LexLink(*wire.PrevData)
+		out.PrevData = &prev
+	}
+
+	if len(wire.Ops) > 0 {
+		out.Ops = make([]*atproto.SyncSubscribeRepos_RepoOp, 0, len(wire.Ops))
+		for _, op := range wire.Ops {
+			if op == nil {
+				continue
+			}
+			mapped := &atproto.SyncSubscribeRepos_RepoOp{
+				Action: op.Action,
+				Path:   op.Path,
+			}
+			if op.Cid != nil {
+				c := lexutil.LexLink(*op.Cid)
+				mapped.Cid = &c
+			}
+			if op.Prev != nil {
+				p := lexutil.LexLink(*op.Prev)
+				mapped.Prev = &p
+			}
+			out.Ops = append(out.Ops, mapped)
+		}
+	}
+
+	return out
 }
